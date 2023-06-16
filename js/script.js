a69661f3fae80b33952cac3c3aa35a99d28368d0
Majharul Islam samir <97847943+RGsamir@users.noreply.github.com>
2023-06-16 14:44:11 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// toggle icon navbar
+
+// scroll sections
+window.onscroll = () => {
+
+    // sticky header
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+
+    // animation footer on scroll
+}
